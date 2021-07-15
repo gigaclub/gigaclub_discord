8739c3d2cae7b689c0e1a386638208b815c393b7
Kevin <kevin@gigaclub.net>
2021-07-15 22:39:15 +0200

    imp(gigaclub_discord): added basic functionallity of a discord bot

--- models/res_company.py
+++ models/res_company.py
@@ -6,3 +6,8 @@ class ResCompany(models.Model):
 
     gc_discord_bot_token = fields.Char("Discord Bot Token")
     gc_discord_server_id = fields.Char("Discord Server ID")
+    gc_discord_server_status = fields.Selection([
+        ("started", "Started"),
+        ("stopped", "Stopped"),
+    ], default="stopped")
+
--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import _, fields, models
+import requests
 
 
 class ResConfigSettings(models.TransientModel):
@@ -6,3 +7,12 @@ class ResConfigSettings(models.TransientModel):
 
     gc_discord_bot_token = fields.Char(related="company_id.gc_discord_bot_token", readonly=False)
     gc_discord_server_id = fields.Char(related="company_id.gc_discord_server_id", readonly=False)
+    gc_discord_server_status = fields.Selection(related="company_id.gc_discord_server_status")
+
+    def start_discord_bot(self):
+        domain = self.env['ir.config_parameter'].get_param('web.base.url')
+        requests.post(f"${domain}/discordbot/start")
+
+    def stop_discord_bot(self):
+        domain = self.env['ir.config_parameter'].get_param('web.base.url')
+        requests.post(f"${domain}/discordbot/stop")
