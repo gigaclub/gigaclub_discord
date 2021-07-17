77951d58849068045c9b2dfefcfc9db9aa2620f4
Kevin <kevin@gigaclub.net>
2021-07-17 15:45:13 +0200

    imp(gigaclub_discord): move discord bot to extra module

--- models/res_company.py
+++ models/res_company.py
@@ -1,13 +1,15 @@
-from odoo import fields, models, api
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     gc_discord_bot_token = fields.Char("Discord Bot Token")
     gc_discord_server_id = fields.Char("Discord Server ID")
-    gc_discord_server_status = fields.Selection([
-        ("started", "Started"),
-        ("stopped", "Stopped"),
-    ], default="stopped")
-
+    gc_discord_bot_status = fields.Selection(
+        [
+            ("started", "Started"),
+            ("stopped", "Stopped"),
+        ],
+        default="stopped",
+    )
--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -1,18 +1,13 @@
-from odoo import _, fields, models
-import requests
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
-    gc_discord_bot_token = fields.Char(related="company_id.gc_discord_bot_token", readonly=False)
-    gc_discord_server_id = fields.Char(related="company_id.gc_discord_server_id", readonly=False)
-    gc_discord_server_status = fields.Selection(related="company_id.gc_discord_server_status")
-
-    def start_discord_bot(self):
-        domain = self.env['ir.config_parameter'].get_param('web.base.url')
-        requests.post(f"${domain}/discordbot/start")
-
-    def stop_discord_bot(self):
-        domain = self.env['ir.config_parameter'].get_param('web.base.url')
-        requests.post(f"${domain}/discordbot/stop")
+    gc_discord_bot_token = fields.Char(
+        related="company_id.gc_discord_bot_token", readonly=False
+    )
+    gc_discord_server_id = fields.Char(
+        related="company_id.gc_discord_server_id", readonly=False
+    )
+    gc_discord_bot_status = fields.Selection(related="company_id.gc_discord_bot_status")
